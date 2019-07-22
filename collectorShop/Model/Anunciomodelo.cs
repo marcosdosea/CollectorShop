e053276b06f7854ca586a76eb4d00c1c3added64
Magda Amaral <magdaamaral2014@yahoo.com.br>
2019-07-22 19:46:54 -0300

    Finalização da criação de classes no Model.

---
+++ collectorShop/Model/Anunciomodelo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Model
+{
+    public partial class Anunciomodelo
+    {
+        public int CodAnuncio { get; set; }
+        public int CodModelo { get; set; }
+
+    }
+}
---
+++ collectorShop/Model/Trocamodelo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Model
+{
+    public partial class Trocamodelo
+    {
+        public int TrocaCodTroca { get; set; }
+        public int ModeloCodModelo { get; set; }
+
+    }
+}
