a25befce4f450fc4e64cd345d64d6b1e06fbb586
Magda Amaral <magdaamaral2014@yahoo.com.br>
2019-07-24 13:51:51 -0300

    Atualização.

---
+++ collectorShop/Services/ServiceException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Services
+{
+    public class ServiceException : SystemException
+    {
+        public ServiceException(String mensagem, Exception inner)
+             : base(mensagem, inner)
+        {
+           
+        }
+        public ServiceException(String mensagem)
+            : base(mensagem)
+        {
+
+        }
+    }
+}
