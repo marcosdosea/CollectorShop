f2db7f99957a9eac6d0e94f3bb66c66a269e13ce
Magda Amaral <magdaamaral2014@yahoo.com.br>
2019-08-16 09:36:51 -0300

    Criação das views de Subcategoria.

---
+++ collectorShop/Services/IGerenciadorSubcategoria.cs
@@ -0,0 +1,17 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services
+{
+    public interface IGerenciadorSubcategoria
+    {
+        void Editar(Subcategoria subcategoriaModel);
+        int Inserir(Subcategoria subcategoriaModel);
+        Subcategoria Obter(int idSubcategoria);
+        IEnumerable<Subcategoria> ObterPorNome(string nome);
+        IEnumerable<Subcategoria> ObterTodos();
+        void Remover(int idSubcategoria);
+    }
+}
---
+++ collectorShop/collectorShop/Controllers/SubcategoriaController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using Services;
+
+namespace collectorShop.Controllers
+{
+    public class SubcategoriaController : Controller
+    {
+        private readonly IGerenciadorSubcategoria gerenciadorSubcategoria;
+        private readonly IService<Categoria> gerenciadorCategoria;
+
+        public SubcategoriaController(IGerenciadorSubcategoria _gerenciadorSubcategoria, IService<Categoria> _gerenciadorCategoria)
+        {
+            gerenciadorSubcategoria = _gerenciadorSubcategoria;
+            gerenciadorCategoria = _gerenciadorCategoria;
+        }
+        // GET: Subcategoria
+        public ActionResult Index()
+        {
+            return View(gerenciadorSubcategoria.ObterTodos());
+        }
+
+        // GET: Subcategoria/Details/5
+        public ActionResult Details(int id)
+        {
+            Subcategoria subcategoria = gerenciadorSubcategoria.Obter(id);
+            return View(subcategoria);
+        }
+
+        // GET: Subcategoria/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Subcategoria/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Subcategoria subcategoria)
+        {
+            if (ModelState.IsValid)
+            {
+                gerenciadorSubcategoria.Inserir(subcategoria);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(subcategoria);
+        }
+
+        // GET: Subcategoria/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Subcategoria subcategoria = gerenciadorSubcategoria.Obter(id);
+            return View(subcategoria);
+        }
+
+        // POST: Subcategoria/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Subcategoria subcategoria)
+        {
+            if (ModelState.IsValid)
+            {
+                gerenciadorSubcategoria.Editar(subcategoria);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(subcategoria);
+
+        }
+
+        // GET: Subcategoria/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Subcategoria subcategoria = gerenciadorSubcategoria.Obter(id);
+            return View(subcategoria);
+        }
+
+        // POST: Subcategoria/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            gerenciadorSubcategoria.Remover(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
