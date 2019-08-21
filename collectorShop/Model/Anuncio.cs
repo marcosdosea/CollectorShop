7c47f0b21c17e8d03999eba2475be0691390c637
Magda Amaral <magdaamaral2014@yahoo.com.br>
2019-08-20 22:59:15 -0300

    Layout de create Anuncio melhorado. Adicionei novos atributos ao model Anúncio. Falta adicioná-los ao gerenciador Anúncio.

--- collectorShop/Model/Anuncio.cs
+++ collectorShop/Model/Anuncio.cs
@@ -32,9 +32,15 @@ namespace Model
         [Required]
         [Display(Name = "Código do Usuário")]
         public int CodUsuario { get; set; }
-        [StringLength(250)]
-        [Display(Name = "Mensagem")]
-        public string Msg { get; set; }
+        
+        [Display(Name = "Opções de Envio")]
+        public string OpcaoEnvio { get; set; }
+
+        [Display(Name = "Categoria")]
+        public string NomeCategoria { get; set; }
+        
+        [Display(Name = "Subcategoria")]
+        public string NomeSubcategoria { get; set; }
 
     }
 }
