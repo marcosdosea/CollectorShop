25a468382800ce828ac51a40aa5cb5027860fbf4
IgorSantos1996 <igsantos1996@gmail.com>
2019-09-10 08:11:47 -0300

    Arquivos não ignorados

---
+++ collectorShop/Services/IServiceNegociacaoAnuncio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Model;
+using Persistence;
+
+namespace Services
+{
+   public  interface IServiceNegociacaoAnuncio
+    {
+        void Editar(NegociacaoAnuncio NegociacaoAnuncio);
+        (int,int) Inserir(NegociacaoAnuncio NegociacaoAnuncio);
+        NegociacaoAnuncio Obter(int codNegociacao, int codAnuncio);
+        IEnumerable<NegociacaoAnuncio> ObterTodos();
+        void Remover(int codNegociacao, int codAnuncio);
+    }
+}
