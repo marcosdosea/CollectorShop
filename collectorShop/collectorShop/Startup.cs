7df377c0907d55b7820eeae8d4befce951a60d52
Magda Amaral <magdaamaral2014@yahoo.com.br>
2020-01-18 10:48:48 -0300

    Classes de teste de Categoria e Usuário parcial.

--- collectorShop/collectorShop/Startup.cs
+++ collectorShop/collectorShop/Startup.cs
@@ -26,6 +26,12 @@ namespace collectorShop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //------------------------------------------------------------------------------
+     /*       services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration["ConnectionStrings: Default"]);
+            }); */
+//----------------------------------------------------------------------------------------------
             services.Configure<IdentityOptions>(options =>
 
             {
