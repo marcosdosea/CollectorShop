4623a860c7c96077199ac45d1b961e3b7a183e57
Magda Amaral <magdaamaral2014@yahoo.com.br>
2020-01-21 16:13:16 -0300

    TestBase modificado e CategoriaControllerTest recebeu mais métodos de teste.

---
+++ collectorShop/UnidadeTeste/TestBase.cs
@@ -0,0 +1,24 @@
+﻿/*using System;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace UnidadeTeste
+{
+    public abstract class TestBase
+
+    {
+
+        protected bancocollectorContext GetDbContext()
+
+        {
+
+            var options = new DbContextOptionsBuilder<bancocollectorContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            return new bancocollectorContext(options);
+
+        }
+
+    }
+}
+*/
