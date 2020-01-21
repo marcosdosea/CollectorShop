6c1e8b8b1b5531079dc787046b3c259e8029df18
Magda Amaral <magdaamaral2014@yahoo.com.br>
2020-01-21 16:13:59 -0300

    Alterações do visual Studio.

--- collectorShop/UnidadeTeste/TestBase.cs
+++ collectorShop/UnidadeTeste/TestBase.cs
@@ -1,4 +1,4 @@
-﻿/*using System;
+﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -11,14 +11,24 @@ namespace UnidadeTeste
         protected bancocollectorContext GetDbContext()
 
         {
+            
+           var options = new DbContextOptionsBuilder<bancocollectorContext>()
+                                  .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                                  .Options;
+                var context = new bancocollectorContext(options);
 
-            var options = new DbContextOptionsBuilder<bancocollectorContext>()
-                .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+                var novoUsuario = new Tbusuario { CodUsuario = 1, Administrador= "Nao", Cpf = "07115678996", Nome= "Luiz Rocha", Email="biorede2000@hotmail.com", Cidade= "Itabaiana", Estado="Sergipe", Cep="495000000", Senha= "M@caco82"};
+            var novoUsuario2 = new Tbusuario { CodUsuario = 2, Administrador = "Sim", Cpf = "07115678996", Nome = "Luiz Rocha", Email = "biorede2000@hotmail.com", Cidade = "Itabaiana", Estado = "Sergipe", Cep = "495000000", Senha = "M@caco82" };
+            context.Tbusuario.Add(novoUsuario);
+            context.Tbusuario.Add(novoUsuario2);
+            var BrinquedoCategoria = new Tbcategoria { CodCategoria = 1, Nome = "Brinquedo" };
+                var LivroCategoria = new Tbcategoria { CodCategoria = 2, Nome = "Livro" };
+               
+                context.Tbcategoria.Add(BrinquedoCategoria);
+                context.Tbcategoria.Add(LivroCategoria);
+            context.SaveChanges();
 
-            return new bancocollectorContext(options);
-
+            return context;
         }
-
     }
 }
-*/
